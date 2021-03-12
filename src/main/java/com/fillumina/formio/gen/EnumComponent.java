ba902638737750b0a11461a68a74038411c59e1e
Francesco Illuminati <fillumina@gmail.com>
2021-03-12 07:31:45 +0100

    fixed upstream bug with placeholders in select component

--- src/main/java/com/fillumina/formio/gen/EnumComponent.java
+++ src/main/java/com/fillumina/formio/gen/EnumComponent.java
@@ -17,7 +17,7 @@ public class EnumComponent extends StringComponent<EnumComponent> {
 
     private final JSONObject data;
     private Set<String> validValues;
-    
+
     public EnumComponent(String key) {
         super("select", key);
         json.put("dataSrc", "values");
@@ -28,17 +28,27 @@ public class EnumComponent extends StringComponent<EnumComponent> {
     protected ResponseValue innerValidate(List<String> list) {
         for (String s : list) {
             if (!validValues.contains(s)) {
-                return new ResponseValue(getKey(), list, isSingleton(), 
+                return new ResponseValue(getKey(), list, isSingleton(),
                         FormError.ENUM_ITEM_NOT_PRESENT, s);
             }
         }
         return super.innerValidate(list);
     }
-    
+
+    /**
+     * There is a bug (?) in current formio implementation that if a placeholder is set for
+     * type select the specified value would not be considered if set and validation required
+     * complains.
+     */
+    @Override
+    public EnumComponent placeholder(String placeholder) {
+        return this;
+    }
+
     public EnumComponent values(String... values) {
         return values(Arrays.asList(values));
     }
-    
+
     public EnumComponent values(Collection<String> values) {
         this.validValues = new HashSet<>(values);
         JSONArray dataValues = new JSONArray();
@@ -46,7 +56,7 @@ public class EnumComponent extends StringComponent<EnumComponent> {
         data.put("values", dataValues);
         return this;
     }
-    
+
     public EnumComponent values(Map<String, String> values) {
         this.validValues = new HashSet<>(values.keySet());
         JSONArray dataValues = new JSONArray();
@@ -54,7 +64,7 @@ public class EnumComponent extends StringComponent<EnumComponent> {
         data.put("values", dataValues);
         return this;
     }
-    
+
     private JSONObject create(String label, String value) {
         JSONObject entry = new JSONObject();
         entry.put("label", label);
